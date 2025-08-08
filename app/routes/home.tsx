da5bc565ed5b035ac3a7deda800c4c0071508ef0
AlexisIsla <alexis.u.isla@gmail.com>
2025-08-08 11:20:09 -0300

    cafe it txt

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -7,7 +7,7 @@ import {useEffect, useState} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Resumind" },
+    { title: "Cafe IT" },
     { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
